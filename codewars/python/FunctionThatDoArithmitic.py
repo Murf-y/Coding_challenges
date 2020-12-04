1e56bf904bd453f14823ba13df789ffc86b6a1cb
Murf-y <66155122+Murf-y@users.noreply.github.com>
2020-12-04 18:24:38 +0200

    Create FunctionThatDoArithmitic.py

---
+++ codewars/python/FunctionThatDoArithmitic.py
@@ -0,0 +1,9 @@
+def arithmetic(a, b, o):
+    if o == "add":
+       return a + b
+    if o == "subtract":
+       return a- b
+    if o == "multiply":
+       return a * b
+    if o == "divide":
+       return a / b
