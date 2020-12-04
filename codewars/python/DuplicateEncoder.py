968bcc06375fc9d375bb850a8fb655174fc47f08
Murf-y <66155122+Murf-y@users.noreply.github.com>
2020-12-04 18:29:04 +0200

    Create DuplicateEncoder.py

---
+++ codewars/python/DuplicateEncoder.py
@@ -0,0 +1,12 @@
+from collections import Counter
+def duplicate_encode(word):
+    word = word.lower()
+    word = list(word)
+    count = Counter(word)
+    result = ""
+    for char in word:
+        if count[char] == 1:
+            result += "("
+        else:
+            result += ")"
+    return result
